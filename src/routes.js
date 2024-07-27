b3f3ab567cc0d65da373adebad9021d962a6ecbe
Tatik <tatiknafiaturohmah@gmail.com>
2024-07-27 20:33:24 +0700

    first commit

---
+++ src/routes.js
@@ -0,0 +1,38 @@
+/* eslint-disable linebreak-style */
+const {
+  addBookHandler,
+  getAllBooksHandler,
+  getBookByIdHandler,
+  editNoteByIdHandler,
+  deleteBookByIdHandler,
+} = require('./handler');
+
+const routes = [
+  {
+    method: 'POST',
+    path: '/books',
+    handler: addBookHandler,
+  },
+  {
+    method: 'GET',
+    path: '/books',
+    handler: getAllBooksHandler,
+  },
+  {
+    method: 'GET',
+    path: '/books/{id}',
+    handler: getBookByIdHandler,
+  },
+  {
+    method: 'PUT',
+    path: '/books/{id}',
+    handler: editNoteByIdHandler,
+  },
+  {
+    method: 'DELETE',
+    path: '/books/{id}',
+    handler: deleteBookByIdHandler,
+  },
+];
+
+module.exports = routes;
